4dda544b94545c95a670f5e05b941677aa3eb40c
dnejdzlr2 <dnejdzlr2@virnect.com>
2024-11-15 10:38:27 +0900

    woodo-44 test

--- src/main/java/com/example/demo/ch04/service/TicketingService.java
+++ src/main/java/com/example/demo/ch04/service/TicketingService.java
@@ -35,6 +35,7 @@ public class TicketingService {
 		System.out.println("t = " + t);
 		System.out.println("t2222 = " + t);
 		System.out.println("t4444 = " + t);
+		System.out.println("t44444444 = " + t);
 		return reservationRepository.findReservationByPerformanceIdAndRoundAndGateAndLineAndSeatAndNameAndPhoneNumber(
 			t.getPerformanceId(),
 			t.getRound(),
