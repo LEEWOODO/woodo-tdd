31cc0d9acff1e354938377ba41f3cffd4ebf7188
dnejdzlr2 <dnejdzlr2@virnect.com>
2024-11-15 12:13:04 +0900

    must merge 222 to develop

--- src/main/java/com/example/demo/WoodoTddApplication.java
+++ src/main/java/com/example/demo/WoodoTddApplication.java
@@ -8,7 +8,7 @@ public class WoodoTddApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WoodoTddApplication.class, args);
-		System.out.println("Hello World i am staging ");
+		System.out.println("Hello World i must 2 ");
 	}
 
 }
