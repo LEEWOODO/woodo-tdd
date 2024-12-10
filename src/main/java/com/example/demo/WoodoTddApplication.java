b9281a970db016578d686549d8e7aa163a383f53
dnejdzlr2 <dnejdzlr2@virnect.com>
2024-12-10 11:12:20 +0900

    test3 - tttt3123123123

--- src/main/java/com/example/demo/WoodoTddApplication.java
+++ src/main/java/com/example/demo/WoodoTddApplication.java
@@ -9,6 +9,7 @@ public class WoodoTddApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WoodoTddApplication.class, args);
 		System.out.println("Hello World i must 2 ");
+		System.out.println("Hello World i must 3 ");
 	}
 
 }
